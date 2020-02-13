b2e8b999d42cbc0a5ac066064bb24dbcfb50918c
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-02-12 21:44:18 -0500

    All: implemented redirectAPI and redirection from user-data script

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 
 (()=>{
 
-    const sources = ["../passwords.sa/user-data.js", "utility.js", "crypto.js", "ui.js"];
+    const fileName = redirectAPI.getUserDataScriptFileName();
+    const sources = [
+        fileName ? fileName : "user-data.js", 
+        "utility.js", "crypto.js", "ui.js",
+    ];
     for (let source of sources) {
         const script = document.createElement("script");
         script.src = source;
--- redirectAPI.js
+++ redirectAPI.js
@@ -1,12 +1,19 @@
 "use strict";
 
-(()=>{
+const redirectAPI = {
+    
+    APIDataKey: "S. A. Kryukov SHA-2 Password Generator API",
+    storage: sessionStorage,
 
-    const sources = ["../passwords.sa/user-data.js", "utility.js", "crypto.js", "ui.js"];
-    for (let source of sources) {
-        const script = document.createElement("script");
-        script.src = source;
-        document.body.appendChild(script);
-    }
+    redirect: function(applicationPath, userDataScriptFileName) { //called by user side
+        this.storage.setItem(this.APIDataKey, userDataScriptFileName);
+        document.location = applicationPath;
+    }, //redirect
 
-})();
+    getUserDataScriptFileName: function() { //called by application side
+        const fileName = this.storage.getItem(this.APIDataKey);
+        this.storage.removeItem(this.APIDataKey);
+        return fileName;
+    }, //getUserDataScriptFileName
+
+};
