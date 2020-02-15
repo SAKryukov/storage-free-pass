6d8f44ff44b9d1582406f8b97800a1d5fa1da412
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2020-02-15 01:09:43 -0500

    Crypto: generalized and simplified digest code

--- crypto.js
+++ crypto.js
@@ -3,24 +3,24 @@
 const passwordGenerator = (() => {
 
     const hashBits = 256;
-    const maxLength = hashBits / 8;
-    const cryptographicHashAlgorithm = `SHA-${hashBits}`;
+    const cryptographicHashAlgorithmPrefix = "SHA-";
 
     async function digestSHA2(message) {
         const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
-        const hashBuffer = await crypto.subtle.digest(cryptographicHashAlgorithm, msgUint8); // hash the message
+        const hashBuffer = await crypto.subtle.digest(`${cryptographicHashAlgorithmPrefix}${hashBits}`, msgUint8); // hash the message
         return Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
     } //digestSHA2
 
     async function generatePassword(masterPassword, seed, start, length, characterRepertoire, shift, inserts) {
         if (!masterPassword) return "";
+        const arrayOfBytes = await digestSHA2(masterPassword + seed);
+        const maxLength = arrayOfBytes.length;
         if (!start) start = 0;
         start = start % maxLength;
         if (!length) length = maxLength;
         if (length > maxLength) length = maxLength;
         if (!shift) shift = 0;
         shift = shift % characterRepertoire.length;
-        const arrayOfBytes = await digestSHA2(masterPassword + seed);
         let output = "";
         for (let index = start; index < start + length; ++index)
             output += characterRepertoire.charAt((shift + arrayOfBytes[index % maxLength]) % characterRepertoire.length);
