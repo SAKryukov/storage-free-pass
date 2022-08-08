d029aa37315b8c7463880d61260308875dd33ac1
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2022-08-08 17:29:01 -0400

    Code: prototype: fixed initialization vector, improved code

--- code/prototypes/prototype.js
+++ code/prototypes/prototype.js
@@ -1,18 +1,5 @@
 window.onload = () => {
 
-    const encryptionAlgorithm = "AES-GCM";
-    const keyDataSourceFormat = "raw";
-    const keyUsages = ["encrypt", "decrypt"];
-    const decryptionFailure = "Failure to decrypt, invalid data";
-
-    const hashBits = 256;
-    const cryptographicHashAlgorithmPrefix = "SHA-";
-    async function digestSHA2(message) {
-        const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
-        const hashBuffer = await crypto.subtle.digest(`${cryptographicHashAlgorithmPrefix}${hashBits}`, msgUint8); // hash the message
-        return hashBuffer;
-    } //digestSHA2      
-
     const elementSet = {
         password: document.querySelector("#id-password"),
         inputData: document.querySelector("#id-input-data"),
@@ -27,13 +14,24 @@ window.onload = () => {
         makeWrapping: value => value.replaceAll(",", ", "),
     }; //elementSet
 
-    elementSet.showDecryptButton(false);
+    const encryptionAlgorithm = "AES-GCM";
+    const keyDataSourceFormat = "raw";
+    const keyUsages = ["encrypt", "decrypt"];
+    const decryptionFailure = "Failure to decrypt, invalid data";
 
-    const getMessageData = () => {
-        const message = elementSet.inputData.value;
-        const enc = new TextEncoder();
-        return enc.encode(message);
-    } //getMessageData
+    const hashBits = 256;
+    const cryptographicHashAlgorithmPrefix = "SHA-";
+
+    async function digestSHA2(message) {
+        const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+        const hashBuffer = await crypto.subtle.digest(`${cryptographicHashAlgorithmPrefix}${hashBits}`, msgUint8); // hash the message
+        return hashBuffer;
+    } //digestSHA2      
+
+    elementSet.showDecryptButton(false);
+    elementSet.password.oninput = () => elementSet.showDecryptButton(false);
+
+    const getMessageData = () => new TextEncoder().encode(elementSet.inputData.value);
 
     const createKey = async masterPassword => {
         const rawKey = await digestSHA2(masterPassword);
@@ -46,43 +44,45 @@ window.onload = () => {
           );      
     } //createKey
 
-    const encrypt = async (iv, key, messageData) =>
+    const encrypt = async (initializationVector, key, messageData) =>
         await window.crypto.subtle.encrypt(
             {
               name: encryptionAlgorithm,
-              iv: iv
+              iv: initializationVector
             },
             key,
             messageData
           );   
-    const decrypt = async (iv, key, encryptedData) =>
+    const decrypt = async (initializationVector, key, encryptedData) =>
         await window.crypto.subtle.decrypt(
           {
             name: encryptionAlgorithm,
-            iv: iv
+            iv: initializationVector
           },
           key,
           encryptedData
         );
 
     let encrypted = null;
+    let initializationVector = null;
     const buttonHandler = async doEncrypt => {
         const key = await createKey(elementSet.password.value);
-        const iv = await digestSHA2(elementSet.password.value); //SA???
+        if (initializationVector == null)
+            initializationVector = await digestSHA2(performance.now().toString());
         if (doEncrypt) {
-            encrypted = await encrypt(iv, key, getMessageData());
+            encrypted = await encrypt(initializationVector, key, getMessageData());
             const encryptedArray = Array.from(new Uint8Array(encrypted));
             elementSet.result.textContent = elementSet.makeWrapping(encryptedArray.toString());
             elementSet.showDecryptButton(true);    
         } else {
             try {
-                const decrypted = await decrypt(iv, key, encrypted);
+                const decrypted = await decrypt(initializationVector, key, encrypted);
                 let decoder = new TextDecoder();
                 elementSet.result.textContent = decoder.decode(decrypted);    
             } catch {
                 elementSet.result.textContent = decryptionFailure;
-            }
-        } //if
+            } //exception
+        } //if encrypt or decrypt
     } //buttonHandler
 
     elementSet.buttons.encrypt.onclick = () => buttonHandler(true);
